9832a7d7f1246b66e0a97bac4b2e5fc793e582b5
martin-b-07e5 <martin.bergagno@gmail.com>
2025-01-03 05:51:25 -0300

    done ✅4-5_Request_GET

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,28 +1,26 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.entity.Medico;
 import med.voll.api.entity.MedicoDTO;
 import med.voll.api.entity.MedicoListadoDTO;
-import med.voll.api.repository.MedicoRepository;
 import med.voll.api.service.MedicoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 // In the controller, simply delegate the task to the service.
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
+
+  private static final Logger logger = LoggerFactory.getLogger(MedicoController.class);
 
   private final MedicoService medicoService;
 
@@ -38,22 +36,9 @@ public class MedicoController {
     medicoService.registrarMedico(medicoDTO);
   }
 
-
-//  @GetMapping
-//  public List<MedicoListadoDTO> listarMedicosParcial() {
-//    // Implementación para listar medicos parcialmente (solo algunos campos)
-//    return medicoService.listarMedicosParcial();
-//  }
-
-  private static final Logger logger = LoggerFactory.getLogger(MedicoController.class);
-
-
-  // http://localhost:8080/medicos?&size=2
   // http://localhost:8080/medicos?page=0&size=2&sort=nombre,asc
-  // http://localhost:8080/medicos?size=2&sort=nombre,asc
   @GetMapping
-  public Page<MedicoListadoDTO> listarMedicosParcial(
-      @PageableDefault(size = 2, sort = "nombre", direction = Sort.Direction.ASC) Pageable pageable) {
+  public Page<MedicoListadoDTO> listarMedicosParcial(@PageableDefault(size = 2, sort = "nombre", direction = Sort.Direction.ASC) Pageable pageable) {
 
     // Print by console (log)
     logger.info("Página solicitada: {}", pageable.getPageNumber());
@@ -63,6 +48,4 @@ public class MedicoController {
     return medicoService.listarMedicosParcial(pageable);
   }
 
-
 }
-
