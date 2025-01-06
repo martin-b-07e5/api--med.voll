7e579d5dc365dde236e6597318b650023f2ab6e9
martin-b-07e5 <martin.bergagno@gmail.com>
2025-01-06 11:31:05 -0300

    organizing code.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -29,23 +29,22 @@ public class MedicoController {
   private MedicoService medicoService;
 
 
-  // read
-  //  http://localhost:8080/medicos/listar
+  ///  read ----------------------------------------------------------------------
+
   @GetMapping("/listar")
+  //  http://localhost:8080/medicos/listar
   public List<Medico> listarMedicos() {
     return medicoService.listarMedicos();
   }
 
-  // http://localhost:8080/medicos/listarDTO
   @GetMapping("/listarDTO")
+  // http://localhost:8080/medicos/listarDTO
   public List<MedicoListadoDTO> listarDTO() {
     return medicoService.listarDTO();
   }
 
-
-  // http://localhost:8080/medicos
-  // http://localhost:8080/medicos?page=0&size=2&sort=nombre,asc
   @GetMapping
+  // http://localhost:8080/medicos?page=0&size=2&sort=nombre,asc
   public Page<MedicoListadoDTO> listarPaginado(
       @PageableDefault(size = 2, sort = "nombre", direction = Sort.Direction.ASC) Pageable pageable) {
 
@@ -57,33 +56,12 @@ public class MedicoController {
     return medicoService.listarPaginado(pageable);
   }
 
-  // http://localhost:8080/medicos/equivalente
-  // http://localhost:8080/medicos/equivalente?page=0&size=2&sort=nombre,asc
   @GetMapping("/equivalente")
+  // http://localhost:8080/medicos/equivalente?page=0&size=2&sort=nombre,asc
   public Page<MedicoListadoDTO> listarPaginadoEquivalente(
       @PageableDefault(size = 2, sort = "nombre", direction = Sort.Direction.ASC) Pageable pageable) {
     return medicoService.listarPaginadoEquivalente(pageable);
   }
-
-  //  ----------------------------------------------------------------------
-  // create
-  @PostMapping("/old")
-  public void agregarMedico(@RequestBody @Valid MedicoDTO medicoDTO) {
-    medicoService.addMedico(medicoDTO);
-  }
-
-  @PostMapping // Endpoint to add a new doctor
-  public ResponseEntity<Void> addMedico(@RequestBody @Valid MedicoDTO medicoDTO) {
-    // Adds the new doctor's details to the database and gets the created entity with the generated ID
-    Medico medico = medicoService.addMedico(medicoDTO); // Assuming your service returns the created Medico entity with ID
-    URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-        .path("/{id}")
-        .buildAndExpand(medico.getId())  // Builds the URL with the generated ID
-        .toUri();
-
-    return ResponseEntity.created(location).build(); // Returns a 201 Created status with the location URL
-  }
-
 
   @GetMapping("/{id}")  // Endpoint to get a doctor's details by ID
   public ResponseEntity<MedicoDTO> getMedicoById(@PathVariable Long id) {
@@ -96,9 +74,55 @@ public class MedicoController {
     }
   }
 
+  @GetMapping("/inactivos")
+  //  http://localhost:8080/medicos/inactivos devuelve entidades
+  public List<Medico> listarMedicosInactivosEntidades() {
+    return medicoService.listarMedicosInactivosEntidades();
+  }
 
-  //  ----------------------------------------------------------------------
-  // update
+  @GetMapping("/inactivosSimple")
+  // http://localhost:8080/medicos/inactivosSimple  devuelve DTOs
+  public List<MedicoListadoSimpleDTO> listarMedicosInactivosDTO() {
+    return medicoService.listarMedicosInactivosDTO();
+  }
+
+  @GetMapping("/activosSimple")
+  //  http://localhost:8080/medicos/activosSimple?page=0&size=3&sort=id,desc
+  public Page<MedicoListadoSimpleDTO> listarMedicosActivosDTO(
+      @PageableDefault(size = 2, sort = "id") Pageable page) {
+    return medicoService.listarMedicosActivosDTO(page);
+  }
+
+  @GetMapping("/listarPorEstado")
+  // http://localhost:8080/medicos/listarPorEstado?inactivo=true
+  public List<MedicoListadoSimpleDTO> listarMedicosPorEstado(@RequestParam boolean inactivo) {
+    return medicoService.listarMedicosPorEstado(inactivo);
+  }
+
+
+  ///  create ----------------------------------------------------------------------
+
+  @PostMapping("/old")
+  public void agregarMedico(@RequestBody @Valid MedicoDTO medicoDTO) {
+    medicoService.addMedico(medicoDTO);
+  }
+
+  @PostMapping // Endpoint to add a new doctor
+  public ResponseEntity<Void> addMedico(@RequestBody @Valid MedicoDTO medicoDTO) {
+    // Adds the new doctor's details to the database and gets the created entity with the generated ID
+    Medico medico = medicoService.addMedico(medicoDTO);
+
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(medico.getId())  // Builds the URL with the generated ID
+        .toUri();
+
+    return ResponseEntity.created(location).build(); // Returns a 201 Created status with the location URL
+  }
+
+
+  ///  update ----------------------------------------------------------------------
+
   @PutMapping("/old")  // Endpoint to update a doctor's information
   public ResponseEntity actualizarMedicoSimple(@RequestBody @Valid MedicoUpdateDTO medicoUpdateDTO) {
     medicoService.updateMedico_getReferenceById(medicoUpdateDTO);  // Updates the doctor's details in the database
@@ -111,60 +135,27 @@ public class MedicoController {
     Medico medicoActualizado = medicoService.getMedicoById(medicoUpdateDTO.id()); // Fetches the updated doctor from the database
     return ResponseEntity.ok(new MedicoUpdateDTO(medicoActualizado));  // Returns the updated doctor data in the response
   }
-//  ----------------------------------------------------------------------
 
-
-  // delete en DB
-  // http://localhost:8080/medicos/36
-  @DeleteMapping("/{id}")
-  public ResponseEntity eliminarMedicoHard(@PathVariable Long id) {
-    medicoService.eliminarMedicoHard(id);
-    return ResponseEntity.noContent().build();
-  }
-
-  // excluir (delete lógico)
-  // http://localhost:8080/medicos/39/excluirPojo
   @PatchMapping("/{id}/excluirPojo")
+  // http://localhost:8080/medicos/39/excluirPojo
   public void excluirMedicoPojo(@PathVariable Long id) {
     medicoService.excluirMedicoPojo(id);
   }
 
-  // http://localhost:8080/medicos/39/excluirDTO
   @PatchMapping("/{id}/excluirDTO")
+  // http://localhost:8080/medicos/39/excluirDTO
   public void excluirDTO(@PathVariable Long id, @RequestBody MedicoExclusionDTO medicoExclusionDTO) {
     medicoService.excluirMedicoDTO(id, medicoExclusionDTO);
   }
 
 
-  // devuelve entidades
-  //  http://localhost:8080/medicos/inactivos
-  @GetMapping("/inactivos")
-  public List<Medico> listarMedicosInactivosEntidades() {
-    return medicoService.listarMedicosInactivosEntidades();
-  }
+  ///  delete ----------------------------------------------------------------------
 
-  // devuelve DTOs
-  // http://localhost:8080/medicos/inactivosSimple
-  @GetMapping("/inactivosSimple")
-  public List<MedicoListadoSimpleDTO> listarMedicosInactivosDTO() {
-    return medicoService.listarMedicosInactivosDTO();
-  }
-
-  //  http://localhost:8080/medicos/activosSimple?page=0&size=3&sort=id,asc
-  //  http://localhost:8080/medicos/activosSimple?page=0&size=3&sort=id,desc
-  @GetMapping("/activosSimple")
-  public Page<MedicoListadoSimpleDTO> listarMedicosActivosDTO(
-      @PageableDefault(size = 2, sort = "id") Pageable page
-  ) {
-    return medicoService.listarMedicosActivosDTO(page);
-  }
-
-
-  // http://localhost:8080/medicos/listarPorEstado?inactivo=true
-  // http://localhost:8080/medicos/listarPorEstado?inactivo=false
-  @GetMapping("/listarPorEstado")
-  public List<MedicoListadoSimpleDTO> listarMedicosPorEstado(@RequestParam boolean inactivo) {
-    return medicoService.listarMedicosPorEstado(inactivo);
+  @DeleteMapping("/{id}")
+  // http://localhost:8080/medicos/36
+  public ResponseEntity eliminarMedicoHard(@PathVariable Long id) {
+    medicoService.eliminarMedicoHard(id);
+    return ResponseEntity.noContent().build();
   }
 
 
