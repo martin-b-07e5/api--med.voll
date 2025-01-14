bd2e5252dd7c9f5aa0a85aca74e2b8d6c978b93c
martin-b-07e5 <martin.bergagno@gmail.com>
2025-01-14 10:34:56 -0300

    improved get localhost:8080/consultas?page=0&size=3&sort=idConsulta,desc

--- src/main/java/med/voll/api/domain/consulta/ConsultaListadoDTO.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaListadoDTO.java
@@ -1,15 +1,28 @@
 package med.voll.api.domain.consulta;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
 public record ConsultaListadoDTO(
     Long idConsulta,
     Long idMedico,
-    Long idPaciente
+    String nombreMedico,
+    Long idPaciente,
+    String nombrePaciente,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime fechaInicio,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime fechaFin
 ) {
   // Constructor to map Consulta entity to ConsultaListadoDTO
   public ConsultaListadoDTO(Consulta consulta) {
     this(consulta.getIdConsulta(),
         consulta.getMedico().getIdMedico(),
-        consulta.getPaciente().getIdPaciente());
+        consulta.getMedico().getNombre(),  // Obtener el nombre del médico desde la entidad
+        consulta.getPaciente().getIdPaciente(),
+        consulta.getPaciente().getNombre(),
+        consulta.getFecha(),
+        consulta.getFecha().plusHours(1)
+    );
   }
 
 }
